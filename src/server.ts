15e74c7ce1cd57f598d2da4153823edcbd526177
mhrazib121 <mhrazib121@gmail.com>
2023-06-11 12:47:20 +0600

    recheck

--- src/server.ts
+++ src/server.ts
@@ -5,12 +5,12 @@ import config from './config'
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    console.log('Mongoose connect successfully')
+    // console.log('Mongoose connect successfully')
     app.listen(config.port, () => {
-      console.log(`Server listening on port ${config.port}`)
+      // console.log(`Server listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log('Mongoose connection failed')
+    // console.log('Mongoose connection failed')
   }
 }
 main()
