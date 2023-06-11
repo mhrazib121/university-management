665c75b8f3eccf3d96ab2a34031acf5fe9f51656
razib121 <mhrazib121@gmail.com>
2023-06-11 21:43:00 +0600

    log time set

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config'
-import logger from './shared/logger'
+import { errorLogger, successLogger } from './shared/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
-    logger.info('Mongoose connect successfully')
+    successLogger.info('Mongoose connect successfully')
     app.listen(config.port, () => {
-      logger.info(`Server listening on port ${config.port}`)
+      successLogger.info(`Server listening on port ${config.port}`)
     })
   } catch (error) {
-    logger.error('Mongoose connection failed')
+    errorLogger.error('Mongoose connection failed')
   }
 }
 main();
--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -1,13 +1,42 @@
-import winston from 'winston';
+import { createLogger, format, transports } from 'winston';
+import path from 'path';
+const { combine, timestamp, label, printf } = format;
 
-const logger = winston.createLogger({
+const myFormat = printf(({ level, message, label, timestamp }) => {
+    const date = new Date(timestamp);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+
+    return `[${label}] ${level}: ${message} ${date} ${hours}:${minutes}:${seconds}`;
+});
+
+const successLogger = createLogger({
     level: 'info',
-    format: winston.format.json(),
+    format: combine(
+        label({ label: 'University Success' }),
+        timestamp(),
+        myFormat
+    ),
     transports: [
-        new winston.transports.Console(),
-        new winston.transports.File({ filename: 'error.log', level: 'error' }),
-        new winston.transports.File({ filename: 'combined.log' }),
+        new transports.Console(),
+        new transports.File({ filename: path.join(process.cwd(), 'logs', 'winston', 'success.log'), level: 'info' }),
+    ],
+});
+const errorLogger = createLogger({
+    level: 'info',
+    format: combine(
+        label({ label: 'University error' }),
+        timestamp(),
+        myFormat
+    ),
+    transports: [
+        new transports.Console(),
+        new transports.File({ filename: path.join(process.cwd(), 'logs', 'winston', 'error.log'), level: 'error' }),
     ],
 });
 
-export default logger;
+export {
+    successLogger,
+    errorLogger
+};
